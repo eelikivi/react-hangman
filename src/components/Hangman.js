ec5c117f580d4ea92e6fb94a4fa0d429da8b25b7
eelikivi <eelikivi@hotmail.com>
2019-07-17 20:57:21 +0300

    fix img paths, update gitignore

--- src/components/Hangman.js
+++ src/components/Hangman.js
@@ -2,15 +2,15 @@ import React from 'react'
 
 
 const sprites = [
-	"/img/0.png",
-	"/img/0-2.png",
-	"/img/0-1.png",
-	"/img/1.png",
-	"/img/2.png",
-	"/img/3.png",
-	"/img/4.png",
-	"/img/5.png",
-	"/img/6.png",
+	"img/0.png",
+	"img/0-2.png",
+	"img/0-1.png",
+	"img/1.png",
+	"img/2.png",
+	"img/3.png",
+	"img/4.png",
+	"img/5.png",
+	"img/6.png",
 ]
 
 class Hangman extends React.Component {
